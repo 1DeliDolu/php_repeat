73f60191b487b19055834741f1caf5c28d91786f
Mustafa Özdemir <mustafa.ozdemir1408@gmail.com>
2025-07-03 13:12:51 +0200

    variable

--- php/examples/bolum-2/2-degisken-tanimlama.php
+++ php/examples/bolum-2/2-degisken-tanimlama.php
@@ -22,4 +22,57 @@
     echo "<br>";
     echo $sayi1 . ' ' . $sayi2; // 50 100
 
+// --- PHP Değişken Kapsamı (Scope) ---
+// Global ve Local Scope
+$x = 5; // global scope
+
+function testGlobal()
+{
+    global $x;
+    // $x fonksiyon içinde doğrudan kullanılamaz
+    echo "<p>Fonksiyon içinde x: $x</p>";
+}
+testGlobal();
+echo "<p>Fonksiyon dışında x: $x</p>";
+
+function testLocal()
+{
+    $y = 10; // local scope
+    echo "<p>Fonksiyon içinde y: $y</p>";
+}
+testLocal();
+echo "<p>Fonksiyon dışında y: $y</p>"; // Hata verir
+
+// global anahtar kelimesi ile global değişkenlere fonksiyon içinde erişim
+$a = 5;
+$b = 10;
+function topla()
+{
+    global $a, $b;
+    $b = $a + $b;
+}
+topla();
+echo $b; // 15
+
+// $GLOBALS dizisi ile global değişkenlere erişim
+$m = 5;
+$n = 10;
+function topla2()
+{
+    $GLOBALS['n'] = $GLOBALS['m'] + $GLOBALS['n'];
+}
+topla2();
+echo $n; // 15
+
+// static anahtar kelimesi ile fonksiyon çağrıldıkça değeri koruyan değişken
+function sayac()
+{
+    static $sayi = 0;
+    echo $sayi;
+    $sayi++;
+}
+sayac(); // 0
+sayac(); // 1
+sayac(); // 2
+
 ?>
